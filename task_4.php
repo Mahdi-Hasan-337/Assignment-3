9ef55e078cfcecfbbd9873c726e1c2b6413098c6
MahdiHasanSadi <mahdi.lu.cse.53.h@gmail.com>
2023-09-26 13:11:08 +0600

    seventh commit

--- task_4.php
+++ task_4.php
@@ -2,31 +2,40 @@
 
 $studentGrades = array(
     'student1' => array(
-        'Math' => 'A+', 
-        'English' => 'A+', 
+        'Math' => 'A+',
+        'English' => 'A+',
         'Science' => 'A+',
     ),
     'student2' => array(
-        'Math' => 'A+', 
-        'English' => 'C', 
-        'Science' => 'B+',
+        'Math' => 'E',
+        'English' => 'D',
+        'Science' => 'D',
     ),
     'student3' => array(
-        'Math' => 'A+', 
-        'English' => 'A+', 
+        'Math' => 'A+',
+        'English' => 'A+',
         'Science' => 'F',
     ),
 );
 
-function task_4($studentGrades, $studentName){
-    $subjectGrades = array_map('gradeToLetter', $studentGrades[$studentName]);
+task_5($studentGrades);
 
-    if (in_array('F', $subjectGrades)) {
+function task_5($studentGrades) {
+    foreach ($studentGrades as $student => $grades) {
+        $grade = calcGrade($grades);
+        echo "$student : $grade" . PHP_EOL;
+    }
+}
+
+function calcGrade($grades) {
+    $subjectGrades = array_map('gradeToLetter', $grades);
+
+    if (in_array(0.0, $subjectGrades)) {
         return 'F';
     }
 
     $rslt = array_sum($subjectGrades) / count($subjectGrades);
-    
+
     $finalGrade = '';
 
     if ($rslt >= 3.85 && $rslt <= 4.0) {
@@ -56,35 +65,29 @@ function task_4($studentGrades, $studentName){
     return $finalGrade;
 }
 
-function gradeToLetter($grade) {
+function gradeToLetter($grade) { /// grade to point conversion
     switch ($grade) {
-        case 'A+':
-            return 4.0;
-        case 'A':
-            return 3.8;
-        case 'A-':
-            return 3.7;
-        case 'B+':
-            return 3.0;
-        case 'B':
-            return 2.7;
-        case 'B-':
-            return 2.3;
-        case 'C+':
-            return 2.0;
-        case 'C':
-            return 1.7;
-        case 'C-':
-            return 1.3;
-        case 'D+':
-            return 1.0;
-        default:
-            return 0.0;
+    case 'A+':
+        return 4.0;
+    case 'A':
+        return 3.8;
+    case 'A-':
+        return 3.7;
+    case 'B+':
+        return 3.0;
+    case 'B':
+        return 2.7;
+    case 'B-':
+        return 2.3;
+    case 'C+':
+        return 2.0;
+    case 'C':
+        return 1.7;
+    case 'C-':
+        return 1.3;
+    case 'D+':
+        return 1.0;
+    default:
+        return 0.0;
     }
 }
-
-foreach ($studentGrades as $studentName => $grades) {
-    $final_rslt = task_4($studentGrades, $studentName);
-    echo "$studentName : $final_rslt\n";
-}
-?>
